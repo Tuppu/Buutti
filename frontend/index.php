b9520bd6183b6bb93975e85540207e4437632443
Tuppu <Tuppu88@gmail.com>
2021-11-06 17:19:31 +0200

    move  code from index.php to bookEditor.php

--- frontend/index.php
+++ frontend/index.php
@@ -9,23 +9,5 @@ require 'config.php';
 
 include 'php/header.php';
 include 'php/booksList.php';
-
-echo '<H2>Book Editor</H1>';
-echo '<form>';
-echo ' <div id="fields">';
-echo '  <input type="hidden" id="id" name="id">';
-echo '  <label for="title">Title:</label><br>';
-echo '  <input type="text" id="title" name="title"><br>';
-echo '  <label for="author">Author:</label><br>';
-echo '  <input type="text" id="author" name="author"><br>';
-echo '  <label for="description">Description:</label><br>';
-echo '  <textarea id="description" name="description" cols="40" rows="5"></textarea><br>';
-echo ' </div>';
-echo ' <div id="buttons">';
-echo '  <input type="button" onclick="saveBookByID()" id="lsave" name="lsave" value="Save">';
-echo '  <input type="button" onclick="saveBookAsNew()" value="Save New">';
-echo '  <input type="button" onclick="deleteBookByID()" value="Delete">';
-echo ' </div>';
-echo '</form>';
-
+include 'php/bookEditor.php';
 include 'php/footer.php';
